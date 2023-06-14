5b350ae3932c70b5f539f328f78c013e8decf19c
たびチュの狐狸先生 <47691322+senkoraku552@users.noreply.github.com>
2023-06-14 19:48:49 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="min-h-screen flex justify-center items-center bg-slate-200">
-      <div className="flex max-w-3xl mx-auto w-full gap-x-8">
-        <div className="w-1/2">
-          <h2 className="text-2xl font-bold">Apple Products</h2>
-          <ul className="mt-4 flex flex-col gap-y-3">
-            <li className=" px-6 py-4 rounded-lg shadow hover:cursor-pointer bg-slate-800 text-white transition">
-              iPhone
-            </li>
-            <li className="bg-white px-6 py-4 rounded-lg shadow hover:cursor-pointer hover:bg-slate-100 transition">
-              iPad
-            </li>
-            <li className=" px-6 py-4 rounded-lg shadow hover:cursor-pointer bg-slate-800 text-white transition">
-              Mac
-            </li>
-            <li className="bg-white px-6 py-4 rounded-lg shadow hover:cursor-pointer hover:bg-slate-100 transition">
-              Apple TV
-            </li>
-            <li className="bg-white px-6 py-4 rounded-lg shadow hover:cursor-pointer hover:bg-slate-100 transition">
-              HomePod
-            </li>
-          </ul>
-          <p className="mt-3 text-slate-400 text-sm">2 item(s) selected</p>
-        </div>
-        <div className="w-1/2">
-          <h2 className="text-2xl font-bold text-slate-400">
-            Selected Products
-          </h2>
-          <p className="mt-4 text-slate-800 text-lg">iPhone, Mac</p>
+import ListItem from "./ListItem";
+
+class App extends React.Component {
+  state = {
+    products: ["iPhone", "iPad", "Mac", "Apple TV", "HomePod"],
+    productSelect: [],
+  };
+
+  productSelectHandler = (product) => {
+    // console.log(product);
+    const newProductSelected = [...this.state.productSelect];
+
+    if (newProductSelected.includes(product)) {
+      //
+      newProductSelected = this.state.productSelect.filter((productArr) => {
+        return productArr !== product;
+      });
+    } else {
+      //
+      newProductSelected.push(product);
+    }
+  };
+
+  render() {
+    return (
+      <div className="min-h-screen flex justify-center items-center bg-slate-200">
+        <div className="flex max-w-3xl mx-auto w-full gap-x-8">
+          <div className="w-1/2">
+            <h2 className="text-2xl font-bold">Apple Products</h2>
+
+            <ul className="mt-4 flex flex-col gap-y-3">
+              {this.state.products.map((product) => (
+                <ListItem
+                  title={product}
+                  active={this.state.productSelect.includes}
+                  onSelect={this.productSelectHandler}
+                ></ListItem>
+              ))}
+            </ul>
+
+            <p className="mt-3 text-slate-400 text-sm">2 item(s) selected</p>
+          </div>
+          <div className="w-1/2">
+            <h2 className="text-2xl font-bold text-slate-400">
+              Selected Products
+            </h2>
+            <p className="mt-4 text-slate-800 text-lg">iPhone, Mac</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/ListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class ListItem extends React.Component {
+  state = {
+    active: false,
+  };
+
+  toggleActive = () => {
+    this.props.onSelect(this.props.title);
+  };
+
+  render() {
+    return (
+      <li
+        className={`px-6 py-4 rounded-lg shadow hover:cursor-pointer transition ${
+          this.props.active
+            ? "bg-slate-800 text-white"
+            : "bg-white hover:bg-slate-100"
+        }`}
+        onClick={this.toggleActive}
+      >
+        {this.props.title}
+      </li>
+    );
+  }
+}
+
+export default ListItem;
